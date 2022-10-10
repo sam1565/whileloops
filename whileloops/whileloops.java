7f384801cfefb0e063be0a093b76d52881409971
Sam Morgan <sam.morgan@mrjaffesclass.com>
2022-10-10 11:32:01 -0700

    whileloops

---
+++ whileloops/whileloops.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class whileloops here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class whileloops
+{
+    public void run() {
+    int i = 0;
+    while (i < 15) {
+        System.out.println("i is" + i + " but is lees than 15");
+    }
+    int k = 0;
+    while (k > 5) {
+        System.out.println("k is" + k + " but is greater than 5");
+    }
+    int b = 0;
+    int a = 0;
+    while (a < 10) {
+        System.out.println("a is" + a + " but is less than 10");
+    }
+    while (b <= 20) {
+        System.out.println("b is" + b + " but is <= 20");
+    }
+}
+}
